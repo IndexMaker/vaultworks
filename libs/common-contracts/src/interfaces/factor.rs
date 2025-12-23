58122dde4a85cc9647708a6de6772736b3fb82a7
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2025-12-23 01:10:45 +0000

    added fetch methods to Factor

--- libs/common-contracts/src/interfaces/factor.rs
+++ libs/common-contracts/src/interfaces/factor.rs
@@ -25,5 +25,13 @@ sol! {
             uint8[] memory,     // index order
             uint8[] memory,     // index order executed / remaining
             uint8[] memory);    // executed asset quantities
+
+        function fetchMarketData(uint128 vendor_id) external view returns (uint8[] memory, uint8[] memory, uint8[] memory);
+
+        function fetchIndexQuote(uint128 index) external view returns (uint8[] memory);
+
+        function getOrderCount(uint128 index) external view returns (uint128);
+
+        function getOrder(uint128 index, uint128 offset) external view returns (address, uint8[] memory);
     }
 }
